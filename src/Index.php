3aa64af5366190abe55af2c8a25877373571128d
AyushKumarSahu <ayushkumarsahu@cedcoss.com>
2022-02-15 16:10:05 +0530

    First

---
+++ src/Index.php
@@ -0,0 +1,21 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Electricity_Bill Using PHP</title>
+</head>
+<body>
+    <div id="wrapper">
+        <h3>Program to calculate Electricity bill in PHP</h3>
+        <form action="" method="post">
+            <input type="number" name="units" placeholder="Enter your no. of units">
+            <input type="submit" name="unit-submit" value="Submit">
+        </form>
+    </div>
+    <div>
+        <?php echo '<br />' .$result_str; ?>
+</body>
+</html>
