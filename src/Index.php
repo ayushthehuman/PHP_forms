0fea428935659898d0d2c1f67e0d76102a954cee
AyushKumarSahu <ayushkumarsahu@cedcoss.com>
2022-02-15 16:13:42 +0530

    P1

--- src/Index.php
+++ src/Index.php
@@ -1,12 +1,47 @@
-
+<!-- Program 1 -->
+<?php
+// $result_str = $result='';
+if (isset($_POST['unit-submit'])) { //Submit 
+    $units = $_POST['units'];      //Input type  
+    if (!empty($units)) {
+        $result = cal_bill($units);
+        $result_str = 'Total ammount of ' . $units . ' = ' . $result;
+    }
+}
+function cal_bill($units)
+{
+    $unitfirst = 3.50;
+    $unitsecond = 4.00;
+    $unitthird = 5.20;
+    $unitfourth = 6.50;
+    if ($units <= 50) {
+        $bill = $units * $unitfirst;
+    } elseif ($units > 50 && $units <= 100) {
+        $temp = $units * $unitfirst;
+        $remaining_units = $units - 50;
+        $bill = $temp + ($remaining_units * $unitsecond);
+    } elseif ($units > 100 && $units <= 200) {
+        $temp = (50 * 3.5) + (100 * $unitsecond);
+        $remaining_units = $units - 150;
+        $bill = $temp + ($remaining_units * $unitthird);
+    } else {
+        $temp = (50 * 3.5) + (100 * $unitsecond) + (100 * $unitthird);
+        $remaining_units = $units - 250;
+        $bill = $temp + ($remaining_units * $unitfourth);
+    }
+    return number_format((float)$bill, 2, '.', '');
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Electricity_Bill Using PHP</title>
 </head>
+
 <body>
     <div id="wrapper">
         <h3>Program to calculate Electricity bill in PHP</h3>
@@ -16,6 +51,7 @@
         </form>
     </div>
     <div>
-        <?php echo '<br />' .$result_str; ?>
+        <?php echo '<br />' . $result_str; ?>
 </body>
+
 </html>
